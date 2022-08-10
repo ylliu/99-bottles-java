675b308f4fd931172ac89fb3c60e635caee2ee35
ylliu <liuming1908@gmail.com>
2022-08-10 11:03:20 +0800

    make case 2 and default similar only one difference "bottle" and "bottles"

--- src/main/java/Bottle.java
+++ src/main/java/Bottle.java
@@ -20,10 +20,10 @@ public class Bottle {
                         "no more bottles of beer on the wall.\n";
                 break;
             case 2:
-                result = "2 bottles of beer on the wall," +
-                        "2 bottles of beer.\n" +
+                result = number + " bottles of beer on the wall," +
+                        number + " bottles of beer.\n" +
                         "Take one down and pass it around, " +
-                        "1 bottle of beer on the wall.\n";
+                        (number - 1) + " bottle of beer on the wall.\n";
                 break;
             default:
                 result = number + " bottles of beer on the wall," +
