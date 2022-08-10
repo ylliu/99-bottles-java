346c2c84f975c67d3902bdd646d685f5f83864ff
ylliu <liuming1908@gmail.com>
2022-08-10 11:51:29 +0800

    create BottleNumber and copy methods to BottleNumber

--- src/main/java/Bottle.java
+++ src/main/java/Bottle.java
@@ -4,22 +4,10 @@ import java.util.stream.IntStream;
 
 public class Bottle {
     public String verse(int number) {
-        String result;
-        switch (number) {
-            case 0:
-                result = quantity(number).toUpperCase() + " " + container(number) + " of beer on the wall," +
-                        quantity(number) + " " + container(number) + " of beer.\n" +
-                        action(number) +
-                        quantity(successor(number)) + " " + container(number - 1) + " of beer on the wall.\n";
-                break;
-            default:
-                result = quantity(number).toUpperCase() + " " + container(number) + " of beer on the wall," +
-                        quantity(number) + " " + container(number) + " of beer.\n" +
-                        action(number) +
-                        quantity(successor(number)) + " " + container(number - 1) + " of beer on the wall.\n";
-
-        }
-        return result;
+        return new BottleNumber(number).quantity().toUpperCase() + " " + container(number) + " of beer on the wall," +
+                new BottleNumber(number).quantity() + " " + container(number) + " of beer.\n" +
+                action(number) +
+                new BottleNumber(successor(number)).quantity() + " " + container(number - 1) + " of beer on the wall.\n";
     }
 
     private int successor(int number) {
@@ -37,10 +25,7 @@ public class Bottle {
     }
 
     private String quantity(int number) {
-        if (number == 0) {
-            return "no more";
-        }
-        return String.valueOf(number);
+        return new BottleNumber(number).quantity();
     }
 
     private String pronoun(int number) {
@@ -60,5 +45,48 @@ public class Bottle {
     public String verses(int starting, int ending) {
         return IntStream.rangeClosed(ending, starting).boxed().sorted(Collections.reverseOrder()).
                 map(this::verse).collect(Collectors.joining("\n"));
+    }
+}
+
+class BottleNumber {
+    private int number;
+
+    public BottleNumber(int number) {
+        this.number = number;
+    }
+
+    public int successor(int number) {
+        if (number == 0) {
+            return 99;
+        }
+        return number - 1;
+    }
+
+    public String action(int number) {
+        if (number == 0) {
+            return "Go to the store and buy some more, ";
+        }
+        return "Take " + pronoun(number) + " down and pass it around, ";
+    }
+
+    public String quantity() {
+        if (this.number == 0) {
+            return "no more";
+        }
+        return String.valueOf(this.number);
+    }
+
+    public String pronoun(int number) {
+        if (number == 1) {
+            return "it";
+        }
+        return "one";
+    }
+
+    public String container(int number) {
+        if (number == 1) {
+            return "bottle";
+        }
+        return "bottles";
     }
 }
