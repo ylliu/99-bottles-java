2bc508c96264097fc975139b7091333721e48d26
ylliu <liuming1908@gmail.com>
2022-08-10 14:39:06 +0800

    move method to BottleNumber

--- src/main/java/Bottle.java
+++ src/main/java/Bottle.java
@@ -6,12 +6,8 @@ public class Bottle {
     public String verse(int number) {
         return new BottleNumber(number).quantity().toUpperCase() + " " + new BottleNumber(number).container() + " of beer on the wall," +
                 new BottleNumber(number).quantity() + " " + new BottleNumber(number).container() + " of beer.\n" +
-                action(number) +
+                new BottleNumber(number).action() +
                 new BottleNumber(new BottleNumber(number).successor(number)).quantity() + " " + new BottleNumber(number - 1).container() + " of beer on the wall.\n";
-    }
-
-    private String action(int number) {
-        return new BottleNumber(number).action();
     }
 
     public String verses(int starting, int ending) {
